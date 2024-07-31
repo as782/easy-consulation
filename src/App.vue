aa089b418784f2552baffd439a64a794b18b2cd6
wengkaibin <617367465@qq.com>
2024-07-31 20:48:49 +0800

    封装网络请求函数&vant自动导入

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,28 @@
 <script setup lang="ts">
+import { request } from '@/utils/request'
+import type { User } from './types/user'
+import { Button as VanButton } from 'vant'
 import { useUserStore } from './stores'
 
+// 测试，请求拦截器，是否携带token，响应拦截器401拦截到登录地址
+const getUserInfo = async () => {
+  const res = await request('/patient/myUser')
+  console.log(res)
+}
+
+// 测试，响应拦截器，出现非10000的情况，和返回剥离后的数据
 const store = useUserStore()
+const login = async () => {
+  const res = await request<User>('/login/password', 'POST', {
+    mobile: '13211112222',
+    // 密码 abc123456 测试：出现非10000的情况
+    password: 'abc12345'
+  })
+  store.setUser(res.data)
+}
 </script>
 
 <template>
-  <p>{{ store.user }}</p>
-  <button
-    @click="
-      store.setUser({
-        id: '1',
-        mobile: '1',
-        account: '1',
-        avatar: '1',
-        token: '1'
-      })
-    "
-  >
-    登录
-  </button>
-  <button @click="store.delUser()">退出</button>
+  <van-button type="primary" @click="getUserInfo">获取个人信息</van-button>
+  <van-button type="primary" @click="login">登录</van-button>
 </template>
