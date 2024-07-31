d44668e96d60ec53b06cbde734ac729254562e84
wengkaibin <617367465@qq.com>
2024-07-31 18:48:45 +0800

    移动适配，主题定制

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,10 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-
+import 'vant/lib/index.css'
+import './styles/main.scss'
 const app = createApp(App)
 
 app.use(createPinia())
