de9eaa4b380952488b4396b1a45f5737c27e1f28
wengkaibin <617367465@qq.com>
2024-08-01 14:44:30 +0800

    打包svg地图导入svg图标

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { createApp } from 'vue'
 
 import App from './App.vue'
 import router from './router'
+
+import 'virtual:svg-icons-register' // 注册svg图标
+
 import 'vant/lib/index.css'
 import './styles/main.scss'
 import pinia from './stores'
--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,9 @@ import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { VantResolver } from '@vant/auto-import-resolver'
-
+import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
+import path from 'path'
+import process from 'node:process'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -15,6 +17,10 @@ export default defineConfig({
     }),
     Components({
       resolvers: [VantResolver()]
+    }),
+    createSvgIconsPlugin({
+      // 指定图标文件夹，绝对路径（NODE代码）
+      iconDirs: [path.resolve(process.cwd(), 'src/icons')]
     })
   ],
   base: '/',
